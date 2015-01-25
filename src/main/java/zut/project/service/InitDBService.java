2641bd9e7cb851382f0dbcf61e8b90885e5e9b27
lukaasm <lukaasm@gmail.com>
2015-01-25 13:23:37 +0100

    do not create new user and roles everytime now

--- src/main/java/zut/project/service/InitDBService.java
+++ src/main/java/zut/project/service/InitDBService.java
@@ -33,41 +33,41 @@ public class InitDBService {
 	@PostConstruct
 	public void Init()
 	{
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		
-		roleRepository.save(roleUser);
-		
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		
-		roleRepository.save(roleAdmin);
-		
-		User userAdmin = new User();
-		userAdmin.setName("admin");
-		
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleUser);
-		roles.add(roleAdmin);
-		
-		userAdmin.setRoles(roles);
-		
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		userAdmin.setPassword(encoder.encode("admin"));
-
-		userRepository.save( userAdmin );
-		
-		/*Descriptor file1 = new Descriptor();
-		file1.setName("DUMMY FILE1");
-		file1.setUser( userAdmin );
-		
-		descriptorRepository.save(file1);
-
-		Descriptor file2 = new Descriptor();
-		file2.setName("DUMMY FILE1");
-		file2.setUser( userAdmin );
-		
-		descriptorRepository.save(file2);*/
+//		Role roleUser = new Role();
+//		roleUser.setName("ROLE_USER");
+//		
+//		roleRepository.save(roleUser);
+//		
+//		Role roleAdmin = new Role();
+//		roleAdmin.setName("ROLE_ADMIN");
+//		
+//		roleRepository.save(roleAdmin);
+//		
+//		User userAdmin = new User();
+//		userAdmin.setName("admin");
+//		
+//		List<Role> roles = new ArrayList<Role>();
+//		roles.add(roleUser);
+//		roles.add(roleAdmin);
+//		
+//		userAdmin.setRoles(roles);
+//		
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		
+//		userAdmin.setPassword(encoder.encode("admin"));
+//
+//		userRepository.save( userAdmin );
+//		
+//		/*Descriptor file1 = new Descriptor();
+//		file1.setName("DUMMY FILE1");
+//		file1.setUser( userAdmin );
+//		
+//		descriptorRepository.save(file1);
+//
+//		Descriptor file2 = new Descriptor();
+//		file2.setName("DUMMY FILE1");
+//		file2.setUser( userAdmin );
+//		
+//		descriptorRepository.save(file2);*/
 	}
 }
